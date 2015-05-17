1213d94f7d54ce5dd7c0c3581d205fe1b9fee123
mikegw <mike@Mikes-MacBook-Pro.local>
2015-05-17 17:42:25 -0400

    Add Board methods

--- js/board.js
+++ js/board.js
@@ -1,5 +1,40 @@
 ;(function () {
 	Chess = window.Chess = window.Chess || {};
-	
-	var Board = Chess.Board = function () {};
+
+	var Board = Chess.Board = function () {
+		this.buildSquares();
+	};
+
+	Board.prototype.buildSquares = function () {
+		this.squares = [];
+		for (var i = 0; i < 8; i++) {
+			var row = [];
+			for (var i = 0; i < 8; i++) {
+				row.push(new Square());
+			}
+			this.squares.push(row);
+		}
+	};
+
+	Board.prototype.square = function (position) {
+		return this.squares[position.y][position.x];
+	};
+
+	Board.prototype.movePiece = function (start_sq, end_sq) {
+		var piece = this.square(start_sq).piece;
+
+		this.square(start_sq).setPiece(null);
+		this.square(end_sq).setPiece(piece);
+	};
+
+
+	var Square = Chess.Square = function (piece) {
+		this.piece = piece || null;
+	};
+
+	Square.prototype.setPiece = function (piece) {
+		this.piece = piece;
+	};
+
+
 })();
--- js/pieces/pawn.js
+++ js/pieces/pawn.js
@@ -1,14 +1,14 @@
 ;(function () {
 	var Chess = window.Chess = window.Chess || {};
-	
+
 	var Vector = Chess.Utils.Vector;
-	
+
 	var Pawn = Chess.Pawn = function () {
 	};
-	
+
 	Pawn.prototype.deltas = function () {
 		var dir = (this.color === "B" ? 1 : -1)
-		
+
 		return [
 			new Vector(0, dir * 1),
 			new Vector(0, dir * 2),
@@ -16,6 +16,6 @@
 			new Vector(-1, dir * 1),
 		];
 	};
-	
+
 	Chess.Utils.inherits(Chess.Piece, Pawn);
 })();
